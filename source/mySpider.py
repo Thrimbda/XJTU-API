2f2f074a8d5eb9f3c71f812dcd89bc67f2126225
Michael <645092284@qq.com>
2016-09-09 23:22:33 +0800

    delete test file mainly.

--- source/mySpider.py
+++ source/mySpider.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Macpotty
 # @Date:   2016-05-22 15:35:19
-# @Last Modified by:   Macpotty
-# @Last Modified time: 2016-08-26 13:59:27
+# @Last Modified by:   64509
+# @Last Modified time: 2016-09-09 23:19:35
 import requests
 from bs4 import BeautifulSoup
 from collections import deque
@@ -37,9 +37,7 @@ class Spider:
         if record:
             self.fobj = FileModule.FileModule()
 
-    def postForm(self, process=None, autoCollect=True, judgeCondition='', postURL=None, **payload):
-        if judgeCondition != '' and judgeCondition not in self.soup.body.text:
-            return
+    def postForm(self, process=None, autoCollect=True, postURL=None, **payload):
         if autoCollect:
             token = {}
             for i in self.soup.find_all('input', type='hidden', value=True):
@@ -62,24 +60,12 @@ class Spider:
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
 
     def getUrls(self):
-        # for i in self.soup.find_all('a', href=True):
-        try:
-            # print(self.soup.find('div', attrs={'class': 'indent'}), "here")
-            for item in self.soup.find('div', attrs={'class': 'indent'}).find_all('a', href=True):
-                url = item.get('href')
-                if "http" in url:
-                    self.urls.append(url)
-                    print('appended queue --->' + url)
-        except AttributeError as e:
-            print(self.response)
-            print(self.response.status_code)
-            print(e)
-            print("No such class in this page: %s" % self.currUrl)
-        except Exception:
-            raise Exception
+        raise NotImplementedError
 
-    def getStub(self):
-        self.soup.find('a', onclick=True)
+    def getStub(self, name):
+        for item in self.soup.find_all('a', onclick=True):
+            if name in item:
+                return item['href']
         return
 
     def refresh(self):
@@ -108,6 +94,80 @@ class Spider:
                     self.cnt += 1
 
     def mainCtl(self):
+        raise NotImplementedError
+
+    def multiThreadMainCtl(self):
+        threads = []
+        for i in range(4):
+            threads.append(threading.Thread(target=self.mainCtl))
+            threads[i].start()
+
+
+class XJTUSpider(Spider):
+    """docstring for XJTUSpider"""
+    def __init__(self, arg):
+        super(XJTUSpider, self).__init__()
+        self.arg = arg
+
+    def getUrls(self):
+        # for i in self.soup.find_all('a', href=True):
+        try:
+            # print(self.soup.find('div', attrs={'class': 'indent'}), "here")
+            for item in self.soup.find('div', attrs={'class': 'indent'}).find_all('a', href=True):
+                url = item.get('href')
+                if "http" in url:
+                    self.urls.append(url)
+                    print('appended queue --->' + url)
+        except AttributeError as e:
+            print(self.response)
+            print(self.response.status_code)
+            print(e)
+            print("No such class in this page: %s" % self.currUrl)
+        except Exception:
+            raise Exception
+        
+    def teachingAssess(self, token, assessGrade=[1, 1, 1, 1, 2]):
+    """this function is very XJTUic.
+       once administrator change those value of checkboxes.
+       we done.
+
+    use it to do the tiring teaching assessment.
+    :type assessGrades: List[grade]
+    """
+    if '实验教师' in self.soup.body.text:
+        assessments = {1: 'PJDJ0441', 2: 'PJDJ0475', 3: 'PJDJ0476', 4: 'PJDJ0477', 5: 'PJDJ0478', 6: 'PJDJ0479'}
+    else:
+        assessments = {1: 'PJDJ0410', 2: 'PJDJ0411', 3: 'PJDJ0382', 4: 'PJDJ0383', 5: 'PJDJ0412', 6: 'PJDJ0385'}
+    token['ztpj'] = '老师认真负责'
+    token['pgyj'] = '满意'
+    token['sfytj'] = 'true'
+    token['type'] = '2'
+    token['actionType'] = '2'
+    token.pop('zbbm')
+    zbbm = []
+    name = None
+    assessIndex = -1
+    for item in self.soup.find('table', id=True).find_all('input', attrs={'name': True, 'value': True}):
+        if 'pfdj' in item.get('name') and item.get('name') != name:
+            name = item.get('name')
+            assessIndex += 1
+            token[name] = assessments[assessGrade[assessIndex]]
+        if 'qz_' in item.get('name'):
+            token[item.get('name')] = '20'
+        if 'zbbm' in item.get('name'):
+            zbbm.append(item.get('value'))
+    list(set(zbbm))
+    payload = list(token.items())
+    for item in zbbm:
+        payload.append(('zbbm', item))
+    try:
+        url = 'http://ssfw.xjtu.edu.cn/index.portal' + self.soup.find('form', action=True).get('action')
+    except AttributeError as e:
+        print(e)
+        raise requests.HTTPError['NONONO']
+    return payload, url
+
+    def mainCtl(self):
         while self.urls:
             self.openQueue(function=None, autoCollect=False)
             # time.sleep(2)
@@ -120,50 +180,3 @@ class Spider:
                 print('done!')
         if self.fobj is not None:
             self.fobj.fileEnd()
-
-    def multiThreadMainCtl(self):
-        threads = []
-        for i in range(4):
-            threads.append(threading.Thread(target=self.mainCtl))
-            threads[i].start()
-
-    def teachingAssess(self, token, assessGrade=[1, 1, 1, 1, 2]):
-        """this function is very XJTUic.
-           once administrator change those value of checkboxes.
-           we done.
-
-        use it to do the tiring teaching assessment.
-        :type assessGrades: List[grade]
-        """
-        if '实验教师' in self.soup.body.text:
-            assessments = {1: 'PJDJ0441', 2: 'PJDJ0475', 3: 'PJDJ0476', 4: 'PJDJ0477', 5: 'PJDJ0478', 6: 'PJDJ0479'}
-        else:
-            assessments = {1: 'PJDJ0410', 2: 'PJDJ0411', 3: 'PJDJ0382', 4: 'PJDJ0383', 5: 'PJDJ0412', 6: 'PJDJ0385'}
-        token['ztpj'] = '老师认真负责'
-        token['pgyj'] = '满意'
-        token['sfytj'] = 'true'
-        token['type'] = '2'
-        token['actionType'] = '2'
-        token.pop('zbbm')
-        zbbm = []
-        name = None
-        assessIndex = -1
-        for item in self.soup.find('table', id=True).find_all('input', attrs={'name': True, 'value': True}):
-            if 'pfdj' in item.get('name') and item.get('name') != name:
-                name = item.get('name')
-                assessIndex += 1
-                token[name] = assessments[assessGrade[assessIndex]]
-            if 'qz_' in item.get('name'):
-                token[item.get('name')] = '20'
-            if 'zbbm' in item.get('name'):
-                zbbm.append(item.get('value'))
-        list(set(zbbm))
-        payload = list(token.items())
-        for item in zbbm:
-            payload.append(('zbbm', item))
-        try:
-            url = 'http://ssfw.xjtu.edu.cn/index.portal' + self.soup.find('form', action=True).get('action')
-        except AttributeError as e:
-            print(e)
-            raise requests.HTTPError['NONONO']
-        return payload, url
