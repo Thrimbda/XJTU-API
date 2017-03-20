9db2172cd95dfd4145b4d42682d73d979c360057
Michael <645092284@qq.com>
2017-03-20 21:21:48 +0800

    fix test case error

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@
 # @Author: Michael
 # @Date:   2016-12-01 02:11:24
 # @Last Modified by:   Michael
-# @Last Modified time: 2017-03-20 21:16:28
+# @Last Modified time: 2017-03-20 21:21:37
 import os
 # import sys
 # sys.path.append("..")
@@ -11,5 +11,5 @@ from source.mySpider import XJTUSpider
 if __name__ == '__main__':
     mySpider = XJTUSpider('ssfw')
     if mySpider.login(username=os.environ.get('username'), password=os.environ.get('password')) == 0:
-        mySpider.schedule()
+        mySpider.schedule('20162')
         mySpider.logout()
